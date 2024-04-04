816d15649fa8dd376e47fa6bccebff2e36ed6331
iRjeffro <133258599+iRjeffro@users.noreply.github.com>
2024-04-04 00:17:12 +0000

    Fix Deployment Project

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MazeGrid from "./MazeGrid.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MazeGrid />
+  </React.StrictMode>,
+);
